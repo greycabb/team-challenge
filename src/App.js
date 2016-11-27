3912e990e515dfd249af637c7a69436ef5dee52f
greycabb <greycabbage@gmail.com>
2016-11-27 13:02:30 -0800

    set everything necessary for node modules, set up page with create-react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SignUpForm from './TeamSignUp';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App container">
+        <div className="App-header">
+          <h1>Sign Up</h1>
+          <p>Our service is fun and awesome, but you must be 13 years old to join</p>
+          <SignUpForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
