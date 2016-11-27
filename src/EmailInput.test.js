3159b8ccf319838dd13033e104440921b21a9179
Melody Lee <leemeli@uw.edu>
2016-11-27 14:45:31 -0800

    Create email input test

---
+++ src/EmailInput.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import EmailInput from './EmailInput';
+import {shallow} from 'enzyme';
+
+describe('<EmailInput /> component', () => {
+    it('should show required error message for blank field', () => {
+
+    })
+    it('should show invalid error message for invalid input', () => {
+
+    })
+    it('should not show any error message for valid input', () => {
+
+    })
+})
