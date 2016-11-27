08ed0b916b36d52ee71076a9081a2c6039dc3659
greycabb <greycabbage@gmail.com>
2016-11-27 13:23:36 -0800

    closes #1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import SignUpForm from './TeamSignUp';
 class App extends React.Component {
   render() {
     return (
-      <div className="App container">
-        <div className="App-header">
+      <div className="container">
           <h1>Sign Up</h1>
           <p>Our service is fun and awesome, but you must be 13 years old to join</p>
           <SignUpForm />
-        </div>
       </div>
     );
   }
