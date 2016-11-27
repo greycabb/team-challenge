d545631348e8c542101dbc577e8e2a1e155d4436
Melody Lee <leemeli@uw.edu>
2016-11-27 14:21:13 -0800

    closes #2

--- src/EmailInput.js
+++ src/EmailInput.js
@@ -4,25 +4,31 @@ import React from 'react';
  * A component representing a controlled input for an email address
  */
 class EmailInput extends React.Component {
+  constructor(props){
+    super(props);
+    this.isValid = true;
+    this.validate = this.validate.bind(this);
+  }
   validate(currentValue){
     if(currentValue === ''){ //check presence
+      this.isValid = true;
       return {missing: true, isValid: false}
     }
 
     //check email validity
     //pattern comparison from w3c https://www.w3.org/TR/html-markup/input.email.html#input.email.attrs.value.single
-    var valid = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(currentValue)
+    var valid = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(currentValue);
     if(!valid){
+      this.isValid = false;
       return {invalidEmail:true, isValid:false};
     }    
-
+    this.isValid = true;
     return {isValid: true}; //no errors
   }  
 
   handleChange(event){  
     //check validity (to inform parent)
     var isValid = this.validate(event.target.value).isValid;
-
     //what to assign to parent's state
     var stateUpdate = {
       'email': {
@@ -49,7 +55,7 @@ class EmailInput extends React.Component {
         {errors.missing &&
           <p className="help-block error-missing">we need to know your email address</p>
         }
-        {errors.invalid &&
+        {!this.isValid &&
           <p className="help-block error-invalid">this is not a valid email address</p>
         }
       </div>
